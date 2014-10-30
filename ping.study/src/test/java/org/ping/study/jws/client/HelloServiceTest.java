c8bf00865cf073932d78f75b31a3a0d2514df4fc
pingoprea <zhuping8802@126.com>
2014-10-30 23:10:57 +0800

    commit

--- ping.study/src/test/java/org/ping/study/jws/client/HelloServiceTest.java
+++ ping.study/src/test/java/org/ping/study/jws/client/HelloServiceTest.java
@@ -1,15 +1,40 @@
 package org.ping.study.jws.client;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
 import jws.client.HelloService;
 import jws.client.HelloService_Service;
 
 public class HelloServiceTest {
 
 	public static void main(String[] args) {
+		HelloServiceTest test = new HelloServiceTest();
+//		test.wsimportTest();
+		test.test();
+	}
+	
+	public void wsimportTest(){
 		HelloService_Service service = new HelloService_Service();
 		HelloService helloService = service.getHelloServicePort();
 		System.out.println(helloService.sayHello("Xiao Min"));
 		System.out.println(helloService.sayHello("Li Si"));
+	}
+	
+	public void test(){
+		try {
+			URL wsdlUrl = new URL("http://localhost:8088/ws/HelloService?wsdl");
+			QName qName = new QName("http://jws/client", "HelloService");
+			Service service = Service.create(wsdlUrl, qName);
+			HelloService hello = service.getPort(HelloService.class);
+			System.out.println(hello.sayHello("zhang san"));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
