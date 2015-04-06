51f404d144d7b1db18af61484b8ac467eef0ccb5
Administrator <Administrator@OEM-20150327XNE>
2015-04-06 15:59:10 +0800

    修改solrServer初始化方法。

--- ping.solr/src/main/java/ping/solr/util/SolrServerUtil.java
+++ ping.solr/src/main/java/ping/solr/util/SolrServerUtil.java
@@ -6,35 +6,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SolrServerUtil {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(SolrServerUtil.class);
 
 	protected ConcurrentUpdateSolrServer solrServer = null;
-	private static final int queeSize = Integer.MAX_VALUE;
-	private static final int threadCount = Runtime.getRuntime()
-			.availableProcessors() * 2;
-	private String solrServerUrl = "";
+	private static final int QUEE_SIZE = Integer.MAX_VALUE;
+	private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors() * 2;
 
-	public void setSolrServerUrl(String solrServerUrl) {
-		this.solrServerUrl = solrServerUrl;
-	}
-
-	public void init() throws Exception {
-		if(StringUtils.isBlank(solrServerUrl)){
-			throw new Exception("SolrServer init error, solrServerUrl is not null.");
+	protected void init(final String url, final int queeSize, final int threadCount) throws Exception {
+		if (StringUtils.isBlank(url)) {
+			throw new Exception("SolrServer init error, url is not null.");
 		}
+		
 		if (solrServer == null) {
 			try {
-				solrServer = new ConcurrentUpdateSolrServer(solrServerUrl,
-						queeSize, threadCount);
+				solrServer = new ConcurrentUpdateSolrServer(url, queeSize <= 0 ? QUEE_SIZE : queeSize,
+						threadCount <= 0 ? THREAD_COUNT : threadCount);
 			} catch (Exception e) {
 				logger.error("init solrServer error", e);
 			}
 		}
 	}
-	
-	public void destory(){
-		if(solrServer != null){
+
+	protected void destory() {
+		if (solrServer != null) {
 			solrServer.shutdown();
 			solrServer = null;
 		}
